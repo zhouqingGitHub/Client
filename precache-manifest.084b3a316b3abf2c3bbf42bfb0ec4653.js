8cb6e0f01be7383e03228e5cf5d5cdacdbca58fe
lijianwen <lijw@mama.cn>
2019-11-13 18:24:11 +0800

    Updates

---
+++ precache-manifest.084b3a316b3abf2c3bbf42bfb0ec4653.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b28db7efe55eef244463a1c59ee1e0",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "b9f84e26382ecdd9e14b",
+    "url": "/react-tutorial/static/css/main.69e399e1.chunk.css"
+  },
+  {
+    "revision": "61a7cdebb2eb6a62dbc5",
+    "url": "/react-tutorial/static/js/2.4917db5f.chunk.js"
+  },
+  {
+    "revision": "b9f84e26382ecdd9e14b",
+    "url": "/react-tutorial/static/js/main.74d582ce.chunk.js"
+  },
+  {
+    "revision": "36d0789c570ad6470391",
+    "url": "/react-tutorial/static/js/runtime-main.a058cf5a.js"
+  }
+]);
